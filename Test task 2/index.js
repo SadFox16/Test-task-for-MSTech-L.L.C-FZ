7eed745b4f4fce348e97e078e41e7cf61c9d85b3
Oleg <oleg.s.kerimov@yandex.com>
2025-05-12 11:49:32 +0300

    Fix JS

--- Test task 2/index.js
+++ Test task 2/index.js
@@ -1,25 +1,51 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const videos = document.querySelectorAll(".video-piece");
+  
+//     // Синхронизация всех видео
+//     if (videos.length > 0) {
+//       videos[0].addEventListener("play", () => {
+//         videos.forEach(video => {
+//           video.currentTime = videos[0].currentTime;
+//           video.play();
+//         });
+//       });
+  
+//       videos[0].addEventListener("pause", () => {
+//         videos.forEach(video => {
+//           video.pause();
+//         });
+//       });
+  
+//       videos[0].addEventListener("seeked", () => {
+//         videos.forEach(video => {
+//           video.currentTime = videos[0].currentTime;
+//         });
+//       });
+//     }
+//   });
+
 document.addEventListener("DOMContentLoaded", function () {
-    const videos = document.querySelectorAll(".video-piece");
-  
-    // Синхронизация всех видео
-    if (videos.length > 0) {
+  const videos = document.querySelectorAll(".video-piece");
+
+  // Синхронизация всех видео
+  if (videos.length > 0) {
       videos[0].addEventListener("play", () => {
-        videos.forEach(video => {
-          video.currentTime = videos[0].currentTime;
-          video.play();
-        });
+          videos.forEach(video => {
+              video.currentTime = videos[0].currentTime;
+              video.play();
+          });
       });
-  
+
       videos[0].addEventListener("pause", () => {
-        videos.forEach(video => {
-          video.pause();
-        });
+          videos.forEach(video => {
+              video.pause();
+          });
       });
-  
+
       videos[0].addEventListener("seeked", () => {
-        videos.forEach(video => {
-          video.currentTime = videos[0].currentTime;
-        });
+          videos.forEach(video => {
+              video.currentTime = videos[0].currentTime;
+          });
       });
-    }
-  });
+  }
+});
